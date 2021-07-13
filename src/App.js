5fa51bd701bbc2219e1c59d6ffb948194d0c0da7
edyion <info@edyion.com>
2021-07-13 20:14:23 +0530

    Lifecycle

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 //css
 import "./App.css";
 //components
@@ -6,22 +6,58 @@ import Topbar from "./components/TopBar";
 import Banner from "./components/Banner";
 import Card from "./components/Card";
 import Footer from "./components/Footer";
+import axios from "axios";
 //Mock Data
-import { products } from "./utils/mockData";
+//import { products } from "./utils/mockData";
 
-const App = () => {
-  return (
-    <div>
-      <Topbar />
-      <Banner />
-      <div className="d-flex flex-wrap">
-        {products.map((item, index) => (
-          <Card {...item} />
-        ))}
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      products: [],
+      loader: true,
+    };
+    console.log("Mount Phase constructor");
+  }
+  componentDidMount() {
+    console.log("Mount Phase componentDidMount");
+    axios
+      .get("https://5d76bf96515d1a0014085cf9.mockapi.io/product")
+      .then((res) => {
+        console.log("Response from API", res);
+        this.setState({ products: res.data, loader: false });
+      });
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log("Update Phase shouldComponentUpdate");
+    if (nextState.products.length > 5) {
+      return true;
+    } else return false;
+  }
+  componentDidUpdate() {
+    console.log("Update Phase componentDidUpdate");
+  }
+
+  render() {
+    console.log(" Phase Render");
+    return (
+      <div>
+        <Topbar />
+        <Banner />
+        {this.state.loader ? (
+          <h1>Loading...</h1>
+        ) : (
+          <div className="d-flex flex-wrap">
+            {this.state.products.map((item, index) => (
+              <Card {...item} />
+            ))}
+          </div>
+        )}
+        <Footer />
       </div>
-      <Footer />
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default App;
--- src/components/Banner/index.js
+++ src/components/Banner/index.js
@@ -1,27 +1,74 @@
-import React from "react";
+import React, { Component } from "react";
 //Module css
 import styles from "./Banner.module.css";
 
-const Banner = () => {
-  return (
-    <div className="jumbotron p-4">
-      <h1 className={`display-4 ${styles.lead}`}>Hello, world!</h1>
-      <p className="lead">
-        This is a simple hero unit, a simple jumbotron-style component for
-        calling extra attention to featured con
-      </p>
-      <hr className="my-4" />
-      <p>
-        It uses utility classes for typography and spacing to space content out
-        within the larger container.
-      </p>
-      <p className="lead">
-        <a className="btn btn-primary btn-lg" href="#" role="button">
-          Learn more
-        </a>
-      </p>
-    </div>
-  );
-};
+class Banner extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "USER",
+    };
+    //console.log("First Call - Constructor");
+  }
+
+  updateUser = () => {
+    this.setState({ name: "NEW" }); // 30ms
+  };
+
+  componentDidMount() {
+    // console.log("Third Call componentDidMount");
+  }
+
+  render() {
+    //console.log("Second Call - Render");
+    return (
+      <div className="jumbotron p-4">
+        <h1 className={`display-4 ${styles.lead}`}>
+          Hello, world! {this.state.name}
+        </h1>
+        <p className="lead">
+          This is a simple hero unit, a simple jumbotron-style component for
+          calling extra attention to featured con
+        </p>
+        <hr className="my-4" />
+        <p>
+          It uses utility classes for typography and spacing to space content
+          out within the larger container.
+        </p>
+        <p className="lead">
+          <a
+            onClick={this.updateUser}
+            className="btn btn-primary btn-lg"
+            href="#"
+            role="button"
+          >
+            Change User
+          </a>
+        </p>
+      </div>
+    );
+  }
+}
+// const Banner = () => {
+//   return (
+//     <div className="jumbotron p-4">
+//       <h1 className={`display-4 ${styles.lead}`}>Hello, world!</h1>
+//       <p className="lead">
+//         This is a simple hero unit, a simple jumbotron-style component for
+//         calling extra attention to featured con
+//       </p>
+//       <hr className="my-4" />
+//       <p>
+//         It uses utility classes for typography and spacing to space content out
+//         within the larger container.
+//       </p>
+//       <p className="lead">
+//         <a className="btn btn-primary btn-lg" href="#" role="button">
+//           Learn more
+//         </a>
+//       </p>
+//     </div>
+//   );
+// };
 
 export default Banner;
