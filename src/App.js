fae7559f2ea9fb832808f333bc5904a18e55b241
edyion <info@edyion.com>
2021-07-14 20:27:40 +0530

    Purecomponent

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component, PureComponent } from "react";
 //css
 import "./App.css";
 //components
@@ -16,6 +16,7 @@ class App extends Component {
     this.state = {
       products: [],
       loader: true,
+      showChild: true,
     };
     console.log("Mount Phase constructor");
   }
@@ -29,22 +30,32 @@ class App extends Component {
       });
   }
 
-  shouldComponentUpdate(nextProps, nextState) {
-    console.log("Update Phase shouldComponentUpdate");
-    if (nextState.products.length > 5) {
-      return true;
-    } else return false;
-  }
+  // shouldComponentUpdate(nextProps, nextState) {
+  //   console.log("Update Phase shouldComponentUpdate");
+  //   if (nextState.products.length > 5) {
+  //     return true;
+  //   } else return false;
+  // }
   componentDidUpdate() {
-    console.log("Update Phase componentDidUpdate");
+    // console.log("Update Phase componentDidUpdate");
   }
 
+  handleChild = () => {
+    //this.setState({ showChild: !this.state.showChild })}
+  };
+
   render() {
-    console.log(" Phase Render");
+    console.log("Render", this.state.count);
     return (
       <div>
         <Topbar />
         <Banner />
+        {this.state.showChild ? <Child /> : ""}
+        <button
+          onClick={() => this.setState({ showChild: !this.state.showChild })}
+        >
+          Child SHOW/HIDE
+        </button>
         {this.state.loader ? (
           <h1>Loading...</h1>
         ) : (
@@ -61,3 +72,28 @@ class App extends Component {
 }
 
 export default App;
+
+class Child extends PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: { age: 1 },
+      ages: "",
+    };
+  }
+
+  componentDidMount() {
+    setInterval(() => {
+      this.setState({ count: { age: 123 } });
+    }, 1000);
+  }
+
+  componentWillUnmount() {
+    console.log("Component is closing");
+    // alert("Component is closing")
+  } // Unmounting phase
+  render() {
+    console.log("Child Render");
+    return <h1>Hello Child {this.state.count} </h1>;
+  }
+}
